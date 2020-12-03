1a261c2abab1ff89d543a669fc3b38dc94acc8ff
zgli2011 <42922455+zgli2011@users.noreply.github.com>
2020-12-03 21:40:52 +0800

    init

--- start.go
+++ start.go
@@ -18,8 +18,8 @@ func main() {
 	cmd.Interpreter = "/bin/bash"
 	cmd.ExecuteUser = "asher"
 	cmd.ExecutePath = "/home/asher"
-	cmd.ExecuteScript = "ifconfig"
-	cmd.ExecuteScriptParam = ""
+	cmd.ExecuteScript = "ifconfig && echo $1 $2"
+	cmd.ExecuteScriptParam = "p1 p2"
 	cmd.ScriptTimeOut = 10
 	fmt.Println(cmd.ExecuteCMD())
 }
