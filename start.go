0e9b61e91e38e64d87f28be1e42b8b0ea6741f89
zgli2011 <42922455+zgli2011@users.noreply.github.com>
2020-12-06 11:37:14 +0800

    init

--- start.go
+++ start.go
@@ -14,12 +14,21 @@ func main() {
 	//fmt.Println("解密结果：", decryptCode)
 
 
-	cmd := &core.CmdInfo{}
-	cmd.Interpreter = "/bin/bash"
-	cmd.ExecuteUser = "asher"
-	cmd.ExecutePath = "/opt"
-	cmd.ExecuteScript = "ifconfig && echo $1 $2 && whoami && pwd"
-	cmd.ExecuteScriptParam = "p1 p2"
-	cmd.ScriptTimeOut = 10
-	fmt.Println(cmd.ExecuteCMD())
+	//cmd := &core.CmdInfo{}
+	//cmd.Interpreter = "/bin/bash"
+	//cmd.ExecuteUser = "asher"
+	//cmd.ExecutePath = "/opt"
+	//cmd.ExecuteScript = "ifconfig && echo $1 $2 && whoami && pwd"
+	//cmd.ExecuteScriptParam = "p1 p2"
+	//cmd.ScriptTimeOut = 10
+	//fmt.Println(cmd.ExecuteCMD())
+
+	cmdTime := &core.CmdInfo{}
+	cmdTime.Interpreter = "/bin/bash"
+	cmdTime.ExecuteUser = "asher"
+	cmdTime.ExecutePath = "/opt"
+	cmdTime.ExecuteScript = "sleep 10"
+	cmdTime.ExecuteScriptParam = ""
+	cmdTime.ScriptTimeOut = 2
+	fmt.Println(cmdTime.ExecuteCMDTimeOut())
 }
