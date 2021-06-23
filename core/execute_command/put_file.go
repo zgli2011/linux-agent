c4ccceedea2c484c0306bc09951c2e7f523afb45
zgli2011 <42922455+zgli2011@users.noreply.github.com>
2021-06-23 22:30:08 +0800

    init

--- core/execute_command/put_file.go
+++ core/execute_command/put_file.go
@@ -13,22 +13,37 @@ func PutFile(users string, group string, file string, content string) error {
 		return err
 	} else {
 		defer f.Close()
+		// 写入文件
 		n, _ := f.Seek(0, os.SEEK_END)
-		if _, err := f.WriteAt([]byte(content), n); err == nil {
-			if fileInfo, err := f.Stat(); err == nil {
-				stat := fileInfo.Sys().(*syscall.Stat_t)
-				stat.
-				if user, err := user.Lookup(users); err == nil {
-					if uid, err := strconv.Atoi(user.Uid); err == nil {
-						if gid, err := strconv.Atoi(user.Gid); err == nil {
-							if stat.Uid == uid && stat.Gid == gid {
-								return os.Chown(file, uid, gid)
-							}
-						}
-					}
-				}
-			}
+		_, err := f.WriteAt([]byte(content), n)
+		if err != nil {
+			return err
 		}
-		return err
+		// 获取文件信息
+		fileInfo, err := f.Stat()
+		if err != nil {
+			return err
+		}
+		// 找到用户的信息
+		stat := fileInfo.Sys().(*syscall.Stat_t)
+		user, err := user.Lookup(users)
+		if err != nil {
+			return err
+		}
+		// 找到用户id
+		uid, err := strconv.Atoi(user.Uid)
+		if err != nil {
+			return err
+		}
+		// 找到用户组ID
+		gid, err := strconv.Atoi(user.Gid)
+		if err != nil {
+			return err
+		}
+		//判断前后uid和gid是否一致， 如果不一致则修改为文件的属主信息
+		if stat.Uid == uint32(uid) && stat.Gid == uint32(gid) {
+			return os.Chown(file, uid, gid)
+		}
+		return nil
 	}
 }
